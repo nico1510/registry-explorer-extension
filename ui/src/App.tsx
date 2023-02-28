1ffb382cb354d40729cfce5dab78e4c6ba9211a1
Nico Beck <nicolas.beck@docker.com>
2023-02-28 19:48:37 +0100

    fix docker:dind not prepending library

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -28,11 +28,11 @@ export function App() {
 
   if (reference.includes("sha256:")) {
     const parts = reference.split("@");
-    repo = parts[0];
+    repo = parts[0].includes("/") ? parts[0] : `library/${parts[0]}`;
     digestOrTag = parts[1];
   } else if (reference.includes(":")) {
     const parts = reference.split(":");
-    repo = parts[0];
+    repo = parts[0].includes("/") ? parts[0] : `library/${parts[0]}`;
     digestOrTag = parts[1];
   } else if (reference.includes("/")) {
     repo = reference;
